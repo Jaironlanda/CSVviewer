f50f6acbc9867024bfdcc6d2d39a6765fcfe4c50
Jairon Landa <jaironlanda@yahoo.com>
2020-04-17 20:21:39 +0800

    add documentation

--- load_data.php
+++ load_data.php
@@ -1,41 +1,41 @@
 <?php
-        $location = './csv-data/';
-        $file_name = 'wtp_data.csv';
-        $path = $location.$file_name;
+    //set path 
+    $location = './csv-data/';
+    // File name
+    $file_name = 'wtp_data.csv';
+    // full path and file name
+    $path = $location.$file_name;
+    //set extension name
+    $allow_ext = array('csv');
+    //check file extension 
+    $file_ext = pathinfo($path);
 
-        $allow_ext = array('csv');
-
-        $file_ext = pathinfo($path);
-
-        if (file_exists($path)) {
-            
-                $data = array(
-                    'msg' => 'File exists',
-                    'status' => false
-                );
-                
-
-            if(in_array($file_ext['extension'], $allow_ext)){
-                
-                $data = array(
-                    'msg'    => 'File is exists and file extension is valid.',
-                    'dataFile' => $path,
-                    'status' => true
-                );
-                
-            }else {
-                $data = array(
-                    'msg'    => 'File is exists but file extension is invalid.',
-                    'status' => false
-                );
-                
-            }
+    if (file_exists($path)) {
+            $data = array(
+                'msg'       => 'File exists',
+                'status'    => false
+            );
+        if(in_array($file_ext['extension'], $allow_ext)){
+            $data = array(
+                'msg'       => 'File is exists and file extension is valid.',
+                'dataFile'  => $path,
+                'status'    => true
+            );
         }else {
             $data = array(
-                'msg' => 'File not exists',
+                'msg'    => 'File is exists but file extension is invalid.',
                 'status' => false
             );
             
         }
-        echo json_encode($data);
-    ?>
+    }else {
+        $data = array(
+            'msg'       => 'File not exists',
+            'status'    => false
+        );
+        
+    }
+
+    // return value
+    echo json_encode($data);
+?>
